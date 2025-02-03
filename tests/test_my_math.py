74eaffdf72044486a49637658d4c1c0913bd4f10
Joseph Nordgren <nordgren@andrews.edu>
2025-02-03 12:03:07 -0500

    first change

--- tests/test_my_math.py
+++ tests/test_my_math.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.my_math import sum, multiply, difference
+from src.my_math import sum, multiply, difference, absolute_sum, calculate_birth_year
 
 def test_sum():
     res = sum(1,1)
@@ -9,15 +9,31 @@ def test_sum():
 # Work together
 ## Test multiply
 
+def test_multipy():
+    res = multiply(2, 2)
+    assert res == 4
 
 # Check for understanding
 ## Test difference
 
+def test_difference():
+    res = difference(2, 1)
+    assert res == 1
 
 # Work together
 ## Test absolute sum
 
-
-
+def test_absolute_sum():
+    res = absolute_sum(-2, 4)
+    assert res == 6 
+def test_itshoudlreturnpositivenumber():
+    rest = absolute_sum(1, 2)
+    assert rest == 3
+def test_botharenegativenumbers():
+    res = absolute_sum(-1, -2)
+    assert res == 3
 # Check for understanding
 ## Test calculate age
+
+#def test_calculate_birth_year():
+#    res = calculate_birth_year()
